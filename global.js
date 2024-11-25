79802d9a36261e918caf8fbe95da5039fa01b747
wesleywr <109980970+wesleywr@users.noreply.github.com>
2024-11-25 20:04:06 -0300

    Create global.js

---
+++ global.js
@@ -0,0 +1,16 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json&#39;
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container__texto')
+paragrafo.innerHTML = `Você sabia que o mundo tem
+<span>${dados.total_pessoas_mundo}<span> de pessoas e
+que aproximadamente <span>${dados.total_pessoas_conectadas}<span>
+estão conectadas em alguma rede social e passam em média
+<span>${dados.tempo_medio} <span> horas conectadas.`
+const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+console.log(paragrafo)
+    }
+    visualizarInformacoesGlobais()
